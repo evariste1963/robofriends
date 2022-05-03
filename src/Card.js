8308b121fe69a197e792de24a584c2fb816f5fc2
evariste1963 <evariste@hotmail.co.uk>
2022-05-03 18:48:51 +0100

    lesson 230 -- done

--- src/Card.js
+++ src/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, email, id }) => {
     //this is JSX and can only return 1 thing -- ie everything in 1st div or an array etc -- OR USING REACT.FRAGMENTS
     //tachyon styling included
     <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-      <img alt={id} src={`https://robohash.org/${id}?200x200`} />
+      <img alt={id} src={`https://robohash.org/${id}?bgset=bg1`} />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
