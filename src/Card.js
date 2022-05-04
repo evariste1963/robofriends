93339e48709286ecc97ffdc91c6eb3baa84a75ad
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-05-04 11:31:39 +0100

    Card.css added to size cards -- bgset=bg1 already being used in url

--- src/Card.js
+++ src/Card.js
@@ -1,9 +1,14 @@
 import React from "react";
+import "./Card.css";
 
 const Card = ({ name, email, id }) => {
   return (
-    <div className="tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5">
-      <img alt={id} src={`https://robohash.org/${id}?bgset=bg1`} />
+    <div className='tc grow bg-light-green br3 pa3 ma2 dib bw2 shadow-5'>
+      <img
+        className='cards'
+        alt={id}
+        src={`https://robohash.org/${id}?bgset=bg1`}
+      />
       <div>
         <h2>{name}</h2>
         <p>{email}</p>
