686d3df03a8d868382792c25755f6d4a6f2ccf79
evariste1963 <evariste@hotmail.co.uk>
2022-05-04 21:03:57 +0100

    async fetch robots

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
-import { robots } from "./robots";
+
 import SearchBox from "./SearchBox";
 import "./App.css";
 
@@ -8,9 +8,15 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
+  }
+
+  async componentDidMount() {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+    const data = await response.json();
+    this.setState({ robots: data });
   }
 
   onSearchChange = (event) => {
@@ -22,13 +28,17 @@ class App extends Component {
         .toLowerCase()
         .includes(this.state.searchfield.toLowerCase());
     });
-    return (
-      <div className="tc">
-        <h1 className="f1">RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange} />
-        <CardList robots={filteredRobots} />
-      </div>
-    );
+    if (this.state.robots.length === 0) {
+      return <h2>Loading, please wait.....</h2>;
+    } else {
+      return (
+        <div className="tc">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
+          <CardList robots={filteredRobots} />
+        </div>
+      );
+    }
   }
 }
 
