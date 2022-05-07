7bd23fc2e648fcc215adfe46a71852bda7af971d
evariste1963 <evariste@hotmail.co.uk>
2022-05-07 21:11:24 +0100

    small formatting changes -- complete

--- src/containers/App.js
+++ src/containers/App.js
@@ -38,32 +38,27 @@ class App extends Component {
     this.setState({ searchfield: event.target.value });
   };
   render() {
-    const filteredRobots = this.state.robots.filter((robot) => {
-      return robot.name
-        .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+    const { robots, searchfield, error } = this.state;
+    const filteredRobots = robots.filter((robot) => {
+      return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
-    if (this.state.robots.length === 0 && !this.state.error) {
-      return (
-        <h2 className="tc" style={{ color: "white", paddingTop: "10rem" }}>
-          Loading Robofriends, please wait.....
-        </h2>
-      );
-    } else if (this.state.error) {
-      return <ErrorMessage error={this.state.error.message} />;
-    } else {
-      return (
-        <div className="tc">
-          <div className="sticky">
-            <h1 className="f1">RoboFriends</h1>
-            <SearchBox searchChange={this.onSearchChange} />
-          </div>
-          <Scroll>
-            <CardList robots={filteredRobots} />
-          </Scroll>
+    return !robots.length && !error ? (
+      <h2 className="tc" style={{ color: "white", paddingTop: "10rem" }}>
+        Loading Robofriends, please wait.....
+      </h2>
+    ) : error ? (
+      <ErrorMessage error={error.message} />
+    ) : (
+      <div className="tc">
+        <div className="sticky">
+          <h1 className="f1">RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange} />
         </div>
-      );
-    }
+        <Scroll>
+          <CardList robots={filteredRobots} />
+        </Scroll>
+      </div>
+    );
   }
 }
 
