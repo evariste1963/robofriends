98abe54ea662650b61c44e2c70246467a3a7822d
evariste1963 <65227896+evariste1963@users.noreply.github.com>
2022-05-09 14:08:05 +0100

    ErrorBoundary

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,7 +4,7 @@ import Card from "./Card";
 const CardList = ({ robots }) => {
   return (
     <div>
-      {robots.map((robot) => {
+      {robots.map(robot => {
         return (
           <Card
             key={robot.id}
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import CardList from "../components/CardList";
 import ErrorMessage from "../components/ErrorMessage";
 import SearchBox from "../components/SearchBox";
 import Scroll from "../components/scroll";
+import ErrorBoundary from "../components/ErrorBoundary";
 import "./App.css";
 
 class App extends Component {
@@ -34,32 +35,34 @@ class App extends Component {
     }
   }
 
-  onSearchChange = (event) => {
+  onSearchChange = event => {
     this.setState({ searchfield: event.target.value });
   };
   render() {
     const { robots, searchfield, error } = this.state;
-    const filteredRobots = robots.filter((robot) => {
+    const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     });
     return !robots.length && !error ? (
-      <h2 className="tc" style={{ color: "white", paddingTop: "10rem" }}>
+      <h2 className='tc' style={{ color: "white", paddingTop: "10rem" }}>
         Loading Robofriends, please wait.....
       </h2>
     ) : error ? (
       <ErrorMessage error={error.message} />
     ) : (
-      <div className="tc">
-        <div className="sticky">
-          <h1 className="f1">RoboFriends</h1>
+      <div className='tc'>
+        <div className='sticky'>
+          <h1 className='f1'>RoboFriends</h1>
           <SearchBox searchChange={this.onSearchChange} />
         </div>
         <Scroll>
-          <CardList robots={filteredRobots} />
+          <ErrorBoundary>
+            <CardList robots={filteredRobots} />
+          </ErrorBoundary>
         </Scroll>
         <footer>
-          <h3 id="footnote">
-            coded by <span id="tag">this.me</span>
+          <h3 id='footnote'>
+            coded by <span id='tag'>this.me</span>
           </h3>
         </footer>
       </div>
